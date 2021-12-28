d6a1db7d942afbbe553c4e0469e3099f4fa6d540
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-28 12:36:06 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,22 +1,34 @@
-
-
 try {
-  function calc() {
-    let x1 = prompt("input x1");
-    let sign = prompt("input sign");
-    let x2 = prompt("input x2");
-    if (isNaN(x1) || isNaN(x2)) {
-      throw new Error("Данные некорректны");
+  let a = prompt("input number a");
+  let b = prompt("input number b");
+  let c = prompt("input number c");
+  let x;
+  if (isNaN(a) || isNaN(b) || isNaN(c)) {
+    throw new Error("введено не число");
+  } else if (a == 0 && b != 0) {
+    x = -c / b;
+  } else if (b == 0 && a != 0) {
+    x = Math.sqrt(-c / a);
+  } else if (c == 0 && a != 0) {
+    x = 0;
+    x1 = b / a;
+    alert(x1);
+  } else {
+    D = b * b - 4 * a * c;
+    if (D < 0) {
+      throw new Error("D не может быть <0");
+    } else if (D == 0) {
+      x = (-b / 2) * a;
+    } else {
+      x = (-b + Math.sqrt(D)) / (2 * a);
+      x1 = (-b - Math.sqrt(D)) / (2 * a);
+      alert(x1);
     }
-    document.write(x1 + sign + x2 + "=");
-    let f;
-    let functionStr = "f = function(x){return " + x1 + sign + x2 + "}";
-    eval(functionStr);
-    document.write(f());
   }
-  calc();
-} catch (ex) {
-  alert("Извините, возникла ошибка: " + ex.message);
+  alert(x);
+} catch (e) {
+  alert("Извините, возникла ошибка: " + e.message);
 } finally {
   alert("Пока!");
 }
+
