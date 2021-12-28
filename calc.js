f7e35d89b13173e7a0f315353d8ac4724535b193
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-28 17:00:51 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,5 +1,15 @@
 try {
-  
+  let x1 = prompt("input x1");
+  let sign = prompt("input sign");
+  let x2 = prompt("input x2");
+  if (isNaN(x1) || isNaN(x2)) {
+    throw new Error("неправильнный ввод");
+  }
+  document.write(x1 + sign + x2 + "=");
+  let f;
+  let functionStr = "f = function(x){return " + x1 + sign + x2 + "}";
+  eval(functionStr);
+  document.write(f());
 } catch (e) {
   alert("Извините, возникла ошибка: " + e.message);
 } finally {
