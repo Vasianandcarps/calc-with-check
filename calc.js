e292e4b12c33ded4f7e90300cb5feea2e2bc7475
Vasianandcarps <90087480+Vasianandcarps@users.noreply.github.com>
2021-12-28 12:16:38 +0200

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,22 +1,22 @@
 
 
-  try {
-     
-    function calc() {
-   let x1 = prompt("input x1");
+try {
+  function calc() {
+    let x1 = prompt("input x1");
     let sign = prompt("input sign");
     let x2 = prompt("input x2");
-    if (isNaN(x1)|| isNaN(x2) || isNaN(sign) ){
-        throw new Error("Данные некорректны");
+    if (isNaN(x1) || isNaN(x2)) {
+      throw new Error("Данные некорректны");
     }
-        document.write(x1 + sign + x2 + "=");
-        let f;
-        let functionStr = "f = function(x){return " + x1 + sign + x2 + "}";
-        eval(functionStr);
-        document.write(f());
-      }
- } catch(ex) {
-    alert("Извините, возникла ошибка: " + ex.message);
- } finally {
-    alert( "Пока!");
- }
+    document.write(x1 + sign + x2 + "=");
+    let f;
+    let functionStr = "f = function(x){return " + x1 + sign + x2 + "}";
+    eval(functionStr);
+    document.write(f());
+  }
+  calc();
+} catch (ex) {
+  alert("Извините, возникла ошибка: " + ex.message);
+} finally {
+  alert("Пока!");
+}
